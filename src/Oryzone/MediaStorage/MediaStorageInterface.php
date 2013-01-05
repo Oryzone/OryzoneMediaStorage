338649fdc5e81c405dde9d5f9844b45e80a8f6b4
lmammino <lmammino@oryzone.com>
2013-01-05 10:14:54 +0100

    Improved MediaStorageInterface

--- src/Oryzone/MediaStorage/MediaStorageInterface.php
+++ src/Oryzone/MediaStorage/MediaStorageInterface.php
@@ -2,7 +2,7 @@
 
 namespace Oryzone\MediaStorage;
 
-use Oryzone\MediaStorage\Model\Media;
+use Oryzone\MediaStorage\Model\MediaInterface;
 
 interface MediaStorageInterface
 {
@@ -10,43 +10,43 @@ interface MediaStorageInterface
     /**
      * Stores a media
      *
-     * @param Model\Media $media
+     * @param Model\MediaInterface $media
      */
-    public function storeMedia(Media $media);
+    public function storeMedia(MediaInterface $media);
 
     /**
      * Update media
      *
-     * @param Model\Media $media
+     * @param Model\MediaInterface $media
      */
-    public function updateMedia(Media $media);
+    public function updateMedia(MediaInterface $media);
 
     /**
      * Removes (deletes) a media and connected files
      *
-     * @param Model\Media $media
+     * @param Model\MediaInterface $media
      */
-    public function removeMedia(Media $media);
+    public function removeMedia(MediaInterface $media);
 
     /**
      * Get the url of a media file (if any)
      *
-     * @param  Model\Media $media
+     * @param  Model\MediaInterface $media
      * @param  string|null      $variant
      * @param  array            $options
      *
      * @return string
      */
-    public function getUrl(Media $media, $variant = NULL, $options = array());
+    public function getUrl(MediaInterface $media, $variant = NULL, $options = array());
 
     /**
      * Renders a given media
      *
-     * @param Model\Media $media
+     * @param Model\MediaInterface $media
      * @param null $variant
      * @param array $options
      * @return mixed
      */
-    public function render(Media $media, $variant = NULL, $options = array());
+    public function render(MediaInterface $media, $variant = NULL, $options = array());
 
 }
