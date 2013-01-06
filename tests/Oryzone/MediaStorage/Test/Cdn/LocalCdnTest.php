df7c3827ddfb54cabb63c645e3aae0b9925fecc1
lmammino <lmammino@oryzone.com>
2013-01-06 19:09:11 +0100

    Added disclaimer info in tests

--- tests/Oryzone/MediaStorage/Test/Cdn/LocalCdnTest.php
+++ tests/Oryzone/MediaStorage/Test/Cdn/LocalCdnTest.php
@@ -1,6 +1,15 @@
 <?php
 namespace Oryzone\MediaStorage\Test\Cdn;
 
+/*
+ * This file is part of the Oryzone/MediaStorage package.
+ *
+ * (c) Luciano Mammino <lmammino@oryzone.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 use Oryzone\MediaStorage\Cdn\LocalCdn,
     Oryzone\MediaStorage\Variant\Variant,
     Oryzone\MediaStorage\Model\SimpleMedia;
--- tests/Oryzone/MediaStorage/Test/Context/ContextTest.php
+++ tests/Oryzone/MediaStorage/Test/Context/ContextTest.php
@@ -1,6 +1,15 @@
 <?php
 namespace Oryzone\MediaStorage\Test\Context;
 
+/*
+ * This file is part of the Oryzone/MediaStorage package.
+ *
+ * (c) Luciano Mammino <lmammino@oryzone.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 use Oryzone\MediaStorage\Context\Context,
     Oryzone\MediaStorage\Variant\VariantTree;
 
