211b3aab36503c3f7d41e4bcebf8c70632cbb9b2
lmammino <lmammino@oryzone.com>
2013-01-07 15:32:42 +0100

    Added disclaimer on Media subpackage tests

--- tests/Oryzone/MediaStorage/Test/Model/MediaTest.php
+++ tests/Oryzone/MediaStorage/Test/Model/MediaTest.php
@@ -1,6 +1,15 @@
 <?php
 namespace Oryzone\MediaStorage\Test\Model;
 
+/*
+ * This file is part of the Oryzone/MediaStorage package.
+ *
+ * (c) Luciano Mammino <lmammino@oryzone.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 use Oryzone\MediaStorage\Variant\VariantInterface;
 
 /**
